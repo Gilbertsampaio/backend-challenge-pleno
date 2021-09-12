5a92c0bec8b67ce70c183b61171aab8a63b32282
Gilbertsampaio <sampaiogilbert@gmail.com>
2021-09-12 11:50:12 -0300

    alteração codeigniter 4 para 2

---
+++ application/views/templates/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Projeto de novo site">
+    <meta name="author" content="GIlbert Sampaio">
+    <title>Novo Site</title>
+    <link href="https://getbootstrap.com/docs/5.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link href="https://getbootstrap.com/docs/5.1/examples/dashboard/dashboard.css" rel="stylesheet">
+    <link href="../public/css/estilo.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@5.9.55/css/materialdesignicons.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link rel="icon" href="../public/favicon.ico">
+</head>
+
+<body>
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="logo col-md-3 col-lg-2" href="#"><img src="../public/img/logo.png" /></a>
+
+        <div class="navbar-nav col-md-12">
+            <div class="row">
+
+                <div class="col-md-9">
+                    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+                </div>
+                <div class="nav-item text-nowrap col-md-3">
+                    <a class="nav-link px-3" href="#">Sign out</a>
+                </div>
+
+            </div>
+
+        </div>
+    </header>
